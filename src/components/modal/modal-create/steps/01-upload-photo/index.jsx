5eb30be00bfb7d30311a9f1ad30252bf7958bd74
unknown <tilektursunbaiuulu@gmail.com>
2023-02-24 01:42:26 +0600

    react

---
+++ src/components/modal/modal-create/steps/01-upload-photo/index.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { ReactComponent as SecBoxIcon } from "../../../../../assets/icons/secbox.svg";
+
+const UploadPhotoStep = (props) => {
+  const handleChangeInput = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.readAsDataURL(file);
+
+    reader.onload = () => {
+      props.onStepFinish(reader.result);
+    };
+  };
+
+  return (
+    <div className="modal-create">
+      <div className="modal-create__content">
+        <div className="content__header">
+          <button className="back txt" onClick={() => props.onClose()}>
+            Close
+          </button>
+          <p className="txt bold">Create publication</p>
+        </div>
+
+        <div className="content__post-container">
+          <div className="content__photo">
+            {props.image && (
+              <img
+                className="content__photo-image"
+                src={props.image}
+                alt=""
+                width={400}
+              />
+            )}
+
+            {!props.image && (
+              <>
+                <SecBoxIcon
+                  className="icons"
+                  width="200px"
+                  height="200px"
+                  color="000"
+                />
+                <label htmlFor="file" className="photo__label">
+                  <span className="photo__txt txt bold">
+                    Upload photo from your PC
+                  </span>
+                  <input
+                    className="photo__upload"
+                    type="file"
+                    onChange={handleChangeInput}
+                    name=""
+                    id="file"
+                  />
+                </label>
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UploadPhotoStep;
--- src/components/side-bar/SideBar.jsx
+++ src/components/side-bar/SideBar.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import "./side-bar.css"
+import React, { useEffect, useState } from "react";
+import "./side-bar.css";
 import { ReactComponent as HomeIcon } from "../../assets/icons/panel-icons/home.svg";
 import { ReactComponent as SearchIcon } from "../../assets/icons/panel-icons/search.svg";
 import { ReactComponent as BellIcon } from "../../assets/icons/panel-icons/bell.svg";
@@ -8,67 +8,96 @@ import { ReactComponent as ProfileIcon } from "../../assets/icons/profile.svg";
 import { ReactComponent as MessageIcon } from "../../assets/icons/message.svg";
 import { ReactComponent as BrowserIcon } from "../../assets/icons/browser.svg";
 import { ReactComponent as PlusIcon } from "../../assets/icons/plus.svg";
-import ModalCreate from '../modal/modal-create/ModalCreate.';
+import ModalCreate from "../modal/modal-create";
 
-const SideBar = () => {
+function SideBar() {
+  const [modalActive, setModalActive] = useState(false);
 
-    const [modalActive , setModalActive] = useState(false)
+  useEffect(() => {
+    console.log("this is state", modalActive);
+  }, [modalActive]);
 
-    return (
-        <div className='side-bar'>
-            <div className='side-bar__logo'>
-                <h1 className='logo'>Instagram</h1>
-            </div>
+  function handleClose() {
+    setModalActive("show");
+  }
 
-            <div className='side-bar__panel'>
-                <div className='panel__item'>
-                    <span><HomeIcon className="panel-icons" width="32px" height="32px" /></span>
-                    <p className='item__txt'>Home</p>
-                </div>
+  return (
+    <div className="side-bar">
+      <div className="side-bar__logo">
+        <h1 className="logo">Instagram</h1>
+      </div>
 
-                <div className='panel__item'>
-                    <span><SearchIcon className="panel-icons" width="32px" height="32px" /></span>
-                    <p className='item__txt'>Find</p>
-                </div>
-
-                <div className='panel__item'>
-                    <span><BrowserIcon className="icons" width="32px" height="32px" /></span>
-                    <p className='item__txt'>Interesting</p>
-                </div>
-
-                <div className='panel__item'>
-                    <span><HomeIcon className="panel-icons" width="32px" height="32px" /></span>
-                    <p className='item__txt'>Reels</p>
-                </div>
-
-                <div className='panel__item'>
-                    <span><MessageIcon className="icons" width="32px" height="32px" /></span>
-                    <p className='item__txt'>Messages</p>
-                </div>
-
-                <div className='panel__item'>
-                    <span><BellIcon className="panel-icons" width="32px" height="32px" /></span>
-                    <p className='item__txt'>Notifications</p>
-                </div>
-
-                <div className='panel__item' onClick={() => setModalActive(true)} >
-                    <ModalCreate active={modalActive} setActive={setModalActive} />
-                    <span><PlusIcon className="icons" width="32px" height="32px" /></span>
-                    <p className='item__txt'>Create</p>
-                </div>
-
-                <div className='panel__item'>
-                    <span><ProfileIcon className="icons" width="32px" height="32px" /></span>
-                    <p className='item__txt'>Profile</p>
-                </div>
-            </div>
-
-            <div className='panel__more'>
-                <span><BurgerIcon className="panel-icons" width="32px" height="32px" /></span>
-                <p className='item__txt'>More</p>
-            </div>
+      <div className="side-bar__panel">
+        <div className="panel__item">
+          <span>
+            <HomeIcon className="panel-icons" width="32px" height="32px" />
+          </span>
+          <p className="item__txt">Home</p>
         </div>
-    );
-};
+
+        <div className="panel__item">
+          <span>
+            <SearchIcon className="panel-icons" width="32px" height="32px" />
+          </span>
+          <p className="item__txt">Find</p>
+        </div>
+
+        <div className="panel__item">
+          <span>
+            <BrowserIcon className="icons" width="32px" height="32px" />
+          </span>
+          <p className="item__txt">Interesting</p>
+        </div>
+
+        <div className="panel__item">
+          <span>
+            <HomeIcon className="panel-icons" width="32px" height="32px" />
+          </span>
+          <p className="item__txt">Reels</p>
+        </div>
+
+        <div className="panel__item">
+          <span>
+            <MessageIcon className="icons" width="32px" height="32px" />
+          </span>
+          <p className="item__txt">Messages</p>
+        </div>
+
+        <div className="panel__item">
+          <span>
+            <BellIcon className="panel-icons" width="32px" height="32px" />
+          </span>
+          <p className="item__txt">Notifications</p>
+        </div>
+
+        <div className="panel__item" onClick={() => setModalActive(true)}>
+          {modalActive && <Test onClose={handleClose} />}
+          <span>
+            <PlusIcon className="icons" width="32px" height="32px" />
+          </span>
+          <p className="item__txt">Create</p>
+        </div>
+
+        <div className="panel__item" onClick={() => setModalActive(false)}>
+          <span>
+            <ProfileIcon className="icons" width="32px" height="32px" />
+          </span>
+          <p className="item__txt">Profile</p>
+        </div>
+      </div>
+
+      <div className="panel__more">
+        <span>
+          <BurgerIcon className="panel-icons" width="32px" height="32px" />
+        </span>
+        <p className="item__txt">More</p>
+      </div>
+    </div>
+  );
+}
+
+function Test(props) {
+  return <button onClick={props.onClose}>lkldfkldfklkdfl</button>;
+}
 
 export default SideBar;
